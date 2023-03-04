4ad18a39cb5be7309bb846706cc289d6ecaa6c13
lydia Lee <lilisacat@126.com>
2023-03-04 11:09:28 +0800

    第一个KafkaSpring提交，实现基本的KafkaTemplate.send和Lister

---
+++ src/main/java/com/lydia/KafkaDemoApplication.java
@@ -0,0 +1,40 @@
+package com.lydia;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Lydia Lee
+ * @date: 2023/3/03
+ */
+@SpringBootApplication
+@RestController
+public class KafkaDemoApplication {
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaDemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDemoApplication.class, args);
+    }
+
+    @Autowired
+    private KafkaTemplate<Object, Object> template;
+
+    @GetMapping("/send/{input}")
+    public void sendFoo(@PathVariable String input) {
+        this.template.send("topic_hello", input);
+    }
+
+    @KafkaListener(id = "rest", topics = "topic_hello")
+    public void listen(String input) {
+        logger.info("Kafka Lister gets value: {}", input);
+    }
+}
