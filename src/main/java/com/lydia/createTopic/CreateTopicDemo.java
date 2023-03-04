39016768d8832c0f4542bb5d09dab9f45ef945d7
lydia Lee <lilisacat@126.com>
2023-03-04 16:51:07 +0800

    repackage

--- src/main/java/com/lydia/createTopic/CreateTopicDemo.java
+++ src/main/java/com/lydia/createTopic/CreateTopicDemo.java
@@ -1,4 +1,4 @@
-package com.lydia;
+package com.lydia.createTopic;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
--- src/main/java/com/lydia/firstDemo/KafkaDemoApplication.java
+++ src/main/java/com/lydia/firstDemo/KafkaDemoApplication.java
@@ -1,4 +1,4 @@
-package com.lydia;
+package com.lydia.firstDemo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
